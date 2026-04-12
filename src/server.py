b3266312f3e44d95109b4ff8a25bae9160e8d7e1
c0pson <pc312199@student.polsl.pl>
2026-04-12 15:11:47 +0200

    Update hard coded URL to get_secret

--- src/server.py
+++ src/server.py
@@ -1,12 +1,17 @@
 import requests #type: ignore
 from requests.exceptions import ConnectTimeout #type: ignore
 
+from src.secrets import get_secret
+from src.constants import SECRET_TYPE
+
+PI_BASE = get_secret(SECRET_TYPE.PI_SERVER)["url"]
+
 class PiServer:
     @classmethod
     def get_status(self) -> int:
         try:
             response = requests.get(
-                url="http://100.77.234.85/status"
+                url=f"{PI_BASE}/status"
             )
             return response.status_code
         except ConnectTimeout as e:
