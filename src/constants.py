32e0c756d1266722da79fa8c78a26917dbfdd57a
c0pson <pc312199@student.polsl.pl>
2026-04-13 13:15:43 +0200

    ASCII art for moon phases

--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,8 @@ import os
 
 from enum import StrEnum
 
+# SECRETS
+
 WORKING_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 SECRET_DIR = os.path.join(WORKING_DIR, ".secrets")
 SECRETS_FILE = os.path.join(SECRET_DIR, "tokens.toml")
@@ -11,3 +13,60 @@ class SECRET_TYPE(StrEnum):
     OPEN_WEATHER = "open-weather"
     PI_SERVER = "pi-server"
     YOUTUBE = "youtube"
+
+# MOON PHASES
+
+"""5 September 2013 - new moon from my birthday"""
+LAST_KNOWN_NEW_MOON = (2013, 9, 5) #yyyy/mm/dd
+
+class MOON(StrEnum):
+    FULL_MOON="""    _..._
+  .:::::::.
+ :::::::::::
+ :::::::::::
+ `:::::::::'
+   `':::''"""
+    WAXING_GIBBOUS="""    _..._
+  .' .::::.
+ :  ::::::::
+ :  ::::::::
+ `. '::::::'
+   `-.::''"""
+    LAST_QUARTER="""    _..._
+  .::::  `.
+ ::::::    :
+ ::::::    :
+ `:::::   .'
+   `'::.-'"""
+    WANING_CRESCENT="""    _..._
+  .::'   `.
+ :::       :
+ :::       :
+ `::.     .'
+   `':..-'"""
+    NEW_MOON="""    _..._
+  .'     `.
+ :         :
+ :         :
+ `.       .'
+   `-...-'"""
+    WANING_GIBBOUS="""
+   _..._
+  .::::. `.
+ :::::::.  :
+ ::::::::  :
+ `::::::' .'
+   `'::'-'"""
+    FIRST_QUARTER="""    _..._
+  .'  ::::.
+ :    ::::::
+ :    ::::::
+ `.   :::::'
+   `-.::''"""
+    WAXING_CRESCENT="""
+    _..._
+  .'   `::.
+ :       :::
+ :       :::
+ `.     .::'
+   `-..:''"""
